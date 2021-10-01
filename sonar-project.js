de50aeea4924a11f17e50ed2ab244b152372914e
Vaibhav Gupta <vaibhav.gupta@hexaviewtech.com>
2021-10-01 19:22:58 +0530

    sd

--- sonar-project.js
+++ sonar-project.js
@@ -4,12 +4,8 @@ sonarqubeScanner({
 	serverUrl: 'http://13.235.241.86:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
-	   // 'sonar.projectName': 'nodejs',
 	    'sonar.projectKey': 'nodejs',
-	    'sonar.login': '4ac9e2f36aa6cd2225cb535a7f79b04107bfba22'
-	   // 'sonar.language':'js',
-           // 'sonar.sourceEncoding':'UTF-8',
-           // 'sonar.sources': '.',
+	    'sonar.login': '4ac9e2f36aa6cd2225cb535a7f79b04107bfba22',
 	  //'sonar.tests': 'specs',
           //'sonar.inclusions' : 'src/**'
        },
