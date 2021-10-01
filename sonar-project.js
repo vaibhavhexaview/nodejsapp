1880ae94abe9c903955f9ee70628ac6835ba2f68
Vaibhav Gupta <vaibhav.gupta@hexaviewtech.com>
2021-10-01 18:39:57 +0530

    Sonar

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://http://13.235.241.86:9000/',
+	serverUrl: 'http://13.235.241.86:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'nodejs',
