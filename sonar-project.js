ca67a09c7b1a550a0541cd90ff53cc3596865a42
Vaibhav Gupta <vaibhav.gupta@hexaviewtech.com>
2021-10-01 18:35:43 +0530

    updated sonar file

--- sonar-project.js
+++ sonar-project.js
@@ -1,13 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.108.59.54:9000/',
+  serverUrl: 'http://http://13.235.241.86:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
-	    'sonar.projectName': 'Node JS Application - Sample',
-	    'sonar.projectKey':'NodeJsMithunTechnologies',
+	    'sonar.projectName': 'nodejs',
 	    'sonar.login': 'admin',
-	    'sonar.password': 'admin',
+	    'sonar.password': 'sonar',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
